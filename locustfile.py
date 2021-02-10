d9cd953f368aca3e873b91653072aa614a2f6659
Cornelius Chakabva <corneyc@gmail.com>
2021-02-10 12:44:01 +0000

    update

--- locustfile.py
+++ locustfile.py
@@ -1,30 +1,20 @@
 import random
-from locust import HttpUser, task, between
-class QuickstartUser(HttpUser):
-    wait_time = between(5, 10)
+from locust import HttpUser, task, TaskSet
+
+class MyTaskSet(HttpUser):
+    
     @task
     def index(self):
         self.client.get("/")
     @task(3)
     def checkPredict(self):
-        self.client.post("/predict", json={
-            "CHAS":{
-                "0":0
-            },
-            "RM":{
-                "0":6.575
-            },
-            "TAX":{
-                "0":296.0
-            },
-            "PTRATIO":{
-                "0":15.3
-            },
-            "B":{
-                "0":396.9
-            },
-            "LSTAT":{
-                "0":4.98
-            }
-        })
+        self.client.post("/predict")
+
+class MyLocust(HttpUser):
+    task_set = MyTaskSet
+
+    min_wait = 1000
+    max_wait = 5000
+
+    
     
