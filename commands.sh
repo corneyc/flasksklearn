275e0658df1f5c5fdf521f46ef8f57cb67afb98c
Cornelius Chakabva <corneyc@gmail.com>
2021-02-10 17:57:10 +0000

    update file

--- commands.sh
+++ commands.sh
@@ -1,4 +1,33 @@
-az webapp up -n $webAppName \
-https://$webAppName.scm.azurewebsites.net/api/logs/docker \
-pyhton3 -m venv ~/.venv &&\
-source ~/.venv/bin/activate
+az login
+
+python3 --version
+
+az --version
+
+az webapp up -n <your-appservice>
+
+git clone https://
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+flask run
+
+az webapp up --sku F1 --name <app-name>
+
+az webapp up
+
+git add .
+
+git commit -m "<your comment">
+
+git push
+
+git pull
+
+az webapp log tail
+
+https://<app-name>.scm.azurewebsites.net/api/logs/docker.
+
+az group delete --no-wait
